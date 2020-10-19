ddae84b936d086edfaa0b36880a79466fba09a20
Dark7lord <budda185@mail.ru>
2020-10-19 22:38:18 +0900

    Удалил дублирование кода в функции фильтра при return

--- js/filter.js
+++ js/filter.js
@@ -11,9 +11,6 @@
       filtered = filtered.filter((apartment) => {
         return apartment.offer.type === housingType.value;
       });
-
-      const result = filtered.slice(0, 5);
-      return result;
     }
 
     const result = filtered.slice(0, 5);

a06036846ad65d397c1fe0f488e84dac00d18deb
Dark7lord <budda185@mail.ru>
2020-10-30 18:34:11 +0900

    Версия отправки несколько фотографии в форме оказалась рабочей (дело было в интернете)

--- js/photo.js
+++ js/photo.js
@@ -30,66 +30,66 @@ avatarChooser.addEventListener(`change`, () => {
 });
 
 imagesChooser.addEventListener(`change`, () => {
-  let images = imagesChooser.files[0];
-  let imagesName = images.name.toLowerCase();
+  // let images = imagesChooser.files[0];
+  // let imagesName = images.name.toLowerCase();
 
-  let matches = FILE_TYPES.some((it) => imagesName.endsWith(it));
+  // let matches = FILE_TYPES.some((it) => imagesName.endsWith(it));
 
-  if (matches) {
-    let reader = new FileReader();
+  // if (matches) {
+  //   let reader = new FileReader();
 
-    reader.addEventListener(`load`, () => {
-      const element = document.createElement(`img`);
-      element.src = reader.result;
+  //   reader.addEventListener(`load`, () => {
+  //     const element = document.createElement(`img`);
+  //     element.src = reader.result;
 
-      element.style.maxWidth = `100%`;
-      element.style.height = `auto`;
-      element.style.maxHeight = `100%`;
-      element.style.display = `block`;
-      element.style.margin = `auto`;
+  //     element.style.maxWidth = `100%`;
+  //     element.style.height = `auto`;
+  //     element.style.maxHeight = `100%`;
+  //     element.style.display = `block`;
+  //     element.style.margin = `auto`;
 
-      imagesPreview.style.display = `flex`;
+  //     imagesPreview.style.display = `flex`;
 
-      imagesPreview.appendChild(element);
-    });
+  //     imagesPreview.appendChild(element);
+  //   });
 
-    reader.readAsDataURL(images);
-  }
+  //   reader.readAsDataURL(images);
+  // }
 
   // Эта была первая версия для загрузки множества картинок, но решил пока
   // закомментировать, так как форма не работает, сделаю пока выбор на одной картинке
   // см. выше
 
-  // let images = imagesChooser.files;
-  // window.data.removeChildrenNode(photoContainer, adFormUpload);
+  let images = imagesChooser.files;
+  window.data.removeChildrenNode(photoContainer, adFormUpload);
 
-  // for (let image of images) {
-  //   let imageName = image.name.toLowerCase();
+  for (let image of images) {
+    let imageName = image.name.toLowerCase();
 
-  //   let matches = FILE_TYPES.some((it) => imageName.endsWith(it));
+    let matches = FILE_TYPES.some((it) => imageName.endsWith(it));
 
-  //   if (matches) {
-  //     let reader = new FileReader();
+    if (matches) {
+      let reader = new FileReader();
 
-  //     reader.addEventListener(`load`, () => {
-  //       const element = document.createElement(`img`);
-  //       const block = imagesPreview.cloneNode(true);
-  //       console.log(block);
+      reader.addEventListener(`load`, () => {
+        const element = document.createElement(`img`);
+        const block = imagesPreview.cloneNode(true);
+        console.log(block);
 
-  //       element.src = reader.result;
-  //       element.style.maxWidth = `100%`;
-  //       element.style.height = `auto`;
-  //       element.style.maxHeight = `100%`;
-  //       element.style.display = `block`;
-  //       element.style.margin = `auto`;
+        element.src = reader.result;
+        element.style.maxWidth = `100%`;
+        element.style.height = `auto`;
+        element.style.maxHeight = `100%`;
+        element.style.display = `block`;
+        element.style.margin = `auto`;
 
-  //       block.style.display = `flex`;
+        block.style.display = `flex`;
 
-  //       block.appendChild(element);
-  //       photoContainer.appendChild(block);
-  //     });
+        block.appendChild(element);
+        photoContainer.appendChild(block);
+      });
 
-  //     reader.readAsDataURL(image);
-  //   }
-  // }
+      reader.readAsDataURL(image);
+    }
+  }
 });
